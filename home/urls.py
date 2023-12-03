56497de5bce9713287ffcce630bc63f09d7fade8
Yash <yashsaygupta@gmail.com>
2023-12-03 15:19:03 +0530

    resume creator completed

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from home.views import *
+ 
+urlpatterns = [
+    path('', home, name = 'home'), 
+    path('resume/', gen_resume, name = 'resume'),
+    path("admin/", admin.site.urls),
+]
